cdb25245cfe2ebe5af473d819a63597a84e3053a
S Praveen Kumar Swamy <praveenks2604@gmail.com>
2025-08-10 15:40:11 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dontenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 dontenv.config();
 
@@ -13,4 +14,6 @@ const app = express();
 
 app.listen(3000, () => {
     console.log("Server is running on 3000")
-})
+});
+
+app.use('/api/user', userRoutes);
